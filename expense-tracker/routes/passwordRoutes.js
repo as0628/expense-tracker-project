13634444238eebe9bcc8f2d4a212f01ca684fefd
Anshu Singh <anshusingh0284087@gmail.com>
2025-09-14 14:55:38 +0530

    expense-tracker project

--- expense-tracker/routes/passwordRoutes.js
+++ expense-tracker/routes/passwordRoutes.js
@@ -1,13 +1,26 @@
+// const express = require("express");
+// const router = express.Router();
+// const { 
+//   forgotPassword, 
+//   resetPasswordForm, 
+//   resetPasswordSubmit 
+// } = require("../controllers/passwordController");
+
+// router.post("/forgotpassword", forgotPassword);
+// router.get("/resetpassword/:id", resetPasswordForm);
+// router.post("/resetpassword/:id", resetPasswordSubmit);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
-const { 
-  forgotPassword, 
-  resetPasswordForm, 
-  resetPasswordSubmit 
-} = require("../controllers/passwordController");
 
-router.post("/forgotpassword", forgotPassword);
+const { resetPasswordForm, resetPasswordSubmit } = require("../controllers/passwordController");
+
+// Route to show reset password page
 router.get("/resetpassword/:id", resetPasswordForm);
+
+// Route to handle reset password submission
 router.post("/resetpassword/:id", resetPasswordSubmit);
 
 module.exports = router;
+
